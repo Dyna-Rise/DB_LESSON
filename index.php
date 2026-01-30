9f7b517408a16dfa8e12938b36ac726f7f00616d
DESKTOP-C5T1MTL\user <174405242+Dyna-Rise@users.noreply.github.com>
2026-01-30 22:29:21 +0900

    div区切り

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>入力フォームページ</h1>
     <p>ここに新規登録の情報を入力してください</p>
-    <label for="user_id">ユーザーID</label>
-    <input type="text" name="user_id" id="user_id">
+
+    <div>
+        <label for="user_id">ユーザーID</label>
+        <input type="text" name="user_id" id="user_id">
+    </div>
+
+    <label for="pass">パスワード</label>
+    <input type="text" name="pass" id="pass">
+
+    <label for="username">お名前</label>
+    <input type="text" name="username" id="username">
+
+    <label for="age">ご年齢</label>
+    <input type="text" name="age" id="age">
+
+    <h2>性別</h2>
+
+    <label for="men">男</label></label>
+    <input type="radio" name="gender" id="men">
+
+    <label for="women">女</label></label>
+    <input type="radio" name="gender" id="women">
+
+    <label for="tell">電話番号</label>
+    <input type="text" name="tell" id="tell">
+
+    <label for="address">住所</label>
+    <input type="text" name="address" id="address">
 
 </body>
+
 </html>
