3419d68afbf2e428582e1100d6f2ba77e209dde5
DESKTOP-C5T1MTL\user <174405242+Dyna-Rise@users.noreply.github.com>
2026-01-30 21:54:52 +0900

    0130-1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>入力フォームページ</h1>
+    <p>ここに新規登録の情報を入力してください</p>
+</body>
+</html>
