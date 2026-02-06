1039bea2f81f7ac6cfe397e257e2470f9edb66b9
DESKTOP-C5T1MTL\user <174405242+Dyna-Rise@users.noreply.github.com>
2026-02-06 21:40:53 +0900

    formタグの追加

--- index.php
+++ index.php
@@ -12,47 +12,49 @@
     <h1>入力フォームページ</h1>
     <p>ここに新規登録の情報を入力してください</p>
 
-    <div>
-        <label for="user_id">ユーザーID</label>
-        <input type="text" name="user_id" id="user_id">
-    </div>
+    <form action="confirm.php" method="post">
+        <div>
+            <label for="user_id">ユーザーID</label>
+            <input type="text" name="user_id" id="user_id">
+        </div>
 
-    <div>
-        <label for="pass">パスワード</label>
-        <input type="text" name="pass" id="pass">
-    </div>
+        <div>
+            <label for="pass">パスワード</label>
+            <input type="text" name="pass" id="pass">
+        </div>
 
-    <div>
-        <label for="username">お名前</label>
-        <input type="text" name="username" id="username">
-    </div>
+        <div>
+            <label for="username">お名前</label>
+            <input type="text" name="username" id="username">
+        </div>
 
-    <div>
-        <label for="age">ご年齢</label>
-        <input type="text" name="age" id="age">
-    </div>
+        <div>
+            <label for="age">ご年齢</label>
+            <input type="text" name="age" id="age">
+        </div>
 
-    <div>
-        <h2>性別</h2>
+        <div>
+            <h2>性別</h2>
 
-        <label for="men">男</label></label>
-        <input type="radio" name="gender" id="men">
+            <label for="men">男</label></label>
+            <input type="radio" name="gender" id="men">
 
-        <label for="women">女</label></label>
-        <input type="radio" name="gender" id="women">
-    </div>
+            <label for="women">女</label></label>
+            <input type="radio" name="gender" id="women">
+        </div>
 
-    <div>
-        <label for="tell">電話番号</label>
-        <input type="text" name="tell" id="tell">
-    </div>
+        <div>
+            <label for="tell">電話番号</label>
+            <input type="text" name="tell" id="tell">
+        </div>
 
-    <div>
-        <label for="address">住所</label>
-        <input type="text" name="address" id="address">
-    </div>
+        <div>
+            <label for="address">住所</label>
+            <input type="text" name="address" id="address">
+        </div>
 
-    <input type="submit" value="登録">
+        <input type="submit" value="登録">
+    </form>
 
 </body>
 
